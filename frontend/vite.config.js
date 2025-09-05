9be3f837360ddd76684afee3f3e870c93b0f1ad8
liudonghua123 <liudonghua123@gmail.com>
2025-09-05 14:59:19 +0800

    fix build for report.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [vue()],
   server: {
@@ -13,6 +15,12 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        report: resolve(__dirname, 'report.html')
+      }
+    }
   }
 })
